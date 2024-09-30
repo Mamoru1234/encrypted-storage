e8e9677e43fe0aed6074a6651753f1cc5bcf0104
Oleksii Gontar <f.gontar5@gmail.com>
2024-09-30 14:28:04 +0300

    improve password prompt

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { createCommand } from 'commander';
 import { decryptFile, encryptFile } from './encrypt.utils';
 import { generateMaterial } from './crypto-material.utils';
-import { readLine } from './read-line-utils';
+import passwordPropmt from '@inquirer/password';
 
 const program = createCommand('encrypted-fs').version('1.0.0');
 
 program
   .command('encrypt <from>')
   .action(async (from) => {
-    const password = await readLine('Your password: ');
+    const password = await passwordPropmt({ message: 'Your password: ' });
     await encryptFile(from, password);
   });
 
 program
   .command('decrypt <from>')
   .action(async (from) => {
-    const password = await readLine('Your password: ');
+    const password = await passwordPropmt({ message: 'Your password: ' });
     await decryptFile(from, password);
   });
 
